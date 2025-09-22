303202c238d26d49a92965b28d1a697e5da87a86
Illuminat3 <matthewrawson2@gmail.com>
2025-09-22 15:56:14 +0100

    Add Agent Login Objects

---
+++ meter-api/meter-api/Datatypes/Responses/AgentLoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace meter_api.Datatypes.Responses
+{
+    public class AgentLoginResponse
+    {
+        public required string MeterId { get; set; }
+        public required string Username { get; set; }
+        public required string LastRequestId { get; set; } = string.Empty;
+    }
+}
