9804835332b6b24ba99cea3732dfa5b490aabde8
Illuminat3 <matthewrawson2@gmail.com>
2025-09-22 16:03:04 +0100

    add di

--- meter-api/meter-api/Program.cs
+++ meter-api/meter-api/Program.cs
@@ -15,6 +15,9 @@ builder.Services.AddSignalR();
 
 // Services
 builder.Services.AddSingleton<IJwtService, JwtService>();
+builder.Services.AddSingleton<IAuthService, AuthService>();
+builder.Services.AddSingleton<IBillingService, BillingService>();
+builder.Services.AddScoped<IDatabaseService, DatabaseService>();
 
 var app = builder.Build();
 
