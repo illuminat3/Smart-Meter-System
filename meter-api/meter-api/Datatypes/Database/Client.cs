d15ed36138db8d8ef4dd86e147ac1baa39dbecf5
Illuminat3 <matthewrawson2@gmail.com>
2025-09-22 16:01:31 +0100

    Add Client Objects

---
+++ meter-api/meter-api/Datatypes/Database/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace meter_api.Datatypes.Database
+{
+    public class Client
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public List<string> MeterIds { get; set; } = [];
+    }
+}
