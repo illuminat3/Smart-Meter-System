7ac070d9c9dc9350311364d6b13751b2dfa8ea62
Illuminat3 <matthewrawson2@gmail.com>
2025-09-22 15:55:47 +0100

    Add Agent Login Endpoint

--- meter-api/meter-api/Controllers/AuthController.cs
+++ meter-api/meter-api/Controllers/AuthController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using meter_api.Datatypes.Requests;
+using meter_api.Datatypes.Responses;
+using meter_api.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace meter_api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class AuthController : ControllerBase
+    public class AuthController(IAuthService authService) : ControllerBase
     {
+        [HttpPost("agent/login")]
+        public async Task<IActionResult> AgentLogin([FromBody] AgentLoginRequest request)
+        {
+            var response = await authService.AgentLogin(request);
+
+            return Ok(response);
+        }
     }
 }
