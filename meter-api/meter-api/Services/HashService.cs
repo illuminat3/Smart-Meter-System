8573e5608feaec78fae2e4e7889cd444546ca877
Illuminat3 <matthewrawson2@gmail.com>
2025-09-23 11:30:55 +0100

    Create HashService

---
+++ meter-api/meter-api/Services/HashService.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+namespace meter_api.Services
+{
+    public class HashService : IHashService
+    {
+        public string GetHash(string rawText)
+        {
+            var bytes = Encoding.UTF8.GetBytes(rawText);
+            var hashBytes = SHA256.HashData(bytes);
+            var hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+            return hash;
+        }
+    }
+}
