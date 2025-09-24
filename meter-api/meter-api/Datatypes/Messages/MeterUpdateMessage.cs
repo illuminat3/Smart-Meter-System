0f00ee0896acffa6e762f7fd193ca5d18244998e
Illuminat3 <matthewrawson2@gmail.com>
2025-09-24 16:20:05 +0100

    Update signalr messages

---
+++ meter-api/meter-api/Datatypes/Messages/MeterUpdateMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace meter_api.Datatypes.Messages
+{
+    public class MeterUpdateMessage
+    {
+        public const string MessageName = "MeterUpdate";
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public required MeterSnapshot Snapshot { get; set; }
+    }
+}
