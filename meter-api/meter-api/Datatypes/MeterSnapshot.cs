2e73f0f417c74cde83200c20691dd93a4e599946
Matthew <matthewrawson2@gmail.com>
2025-09-24 16:22:16 +0100

    Update signalr messages (#34)

--- meter-api/meter-api/Datatypes/MeterSnapshot.cs
+++ meter-api/meter-api/Datatypes/MeterSnapshot.cs
@@ -5,6 +5,7 @@
         public required string MeterId { get; set; }
         public required string DisplayName { get; set; }
         public required double CurrentUsage { get; set; }
-        public required float TotalCost { get; set; }
+        public required double TotalUsage { get; set; }
+        public required decimal TotalCost { get; set; }
     }
 }
