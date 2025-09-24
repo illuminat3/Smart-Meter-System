6ec72044e3c6dd53a67c0de6cb3b7a76c3185c09
Illuminat3 <matthewrawson2@gmail.com>
2025-09-24 17:41:31 +0100

    Add snapshot service

---
+++ meter-api/meter-api/Services/SnapshotService.cs
@@ -0,0 +1,37 @@
+﻿using meter_api.Datatypes;
+
+namespace meter_api.Services
+{
+    public class SnapshotService(IDatabaseService databaseService) : ISnapshotService
+    {
+        public async Task<MeterSnapshot> GetMeterSnapshot(string meterId)
+        {
+            return await databaseService.GetMeterSnapshotFromId(meterId);
+        }
+
+        public async Task<List<MeterSnapshot>> GetMeterSnapshotsForClient(string clientId)
+        {
+            var client = await databaseService.GetClientFromId(clientId);
+            var meterIds = client.MeterIds ?? [];
+            var meterSnapshots = new List<MeterSnapshot>();
+
+            foreach (var meterId in meterIds)
+            {
+                try
+                {
+                    var meterSnapshot = await databaseService.GetMeterSnapshotFromId(meterId);
+                    if (meterSnapshot != null)
+                    {
+                        meterSnapshots.Add(meterSnapshot);
+                    }
+                }
+                catch (KeyNotFoundException)
+                {
+                   
+                }
+            }
+
+            return meterSnapshots;
+        }
+    }
+}
