907e7525e803457d3cc5288d38d76ee4c4556cd2
Matthew Rawson <Matthew.Rawson@infotrack.co.uk>
2025-10-03 17:01:04 +0100

    fiux errors

--- frontend/src/components/InputComponent.vue
+++ frontend/src/components/InputComponent.vue
@@ -1,17 +1,23 @@
 <template>
   <IftaLabel>
-    <slot :id="id"/>
-    <label :for="id">{{ label }} <span v-if="props.validator?.required" class="text-red-500">*</span></label>
+    <slot :id="id" />
+    <label :for="id"
+      >{{ label }}
+      <span v-if="props.validation?.required" class="text-red-500"
+        >*</span
+      ></label
+    >
   </IftaLabel>
 
   <div v-if="validation.$dirty && (validation.$error || validation.$invalid)">
-    <small class="text-red-500" v-for="error in validation.$errors">{{ error.$message }}</small>
+    <small class="text-red-500" v-for="error in validation.$errors">{{
+      error.$message
+    }}</small>
   </div>
 </template>
 
 <script setup lang="ts">
 import IftaLabel from "primevue/iftalabel";
-import Message from "primevue/message";
 
 const props = defineProps<{
   id: string;
