d6cd1e51c002f77878f614b48a086be00b5e344a
Matthew Rawson <Matthew.Rawson@infotrack.co.uk>
2025-10-13 19:17:28 +0100

    Remove Old Messages

--- meter-api/meter-api/Datatypes/Messages/InitialStateMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace meter_api.Datatypes.Messages
-{
-    public class InitialStateMessage : IMessage<List<MeterSnapshot>>
-    {
-        public string MessageName { get; } = "InitialState";
-        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-        public List<MeterSnapshot> Body { get; set; } = [];
-    }
-}
