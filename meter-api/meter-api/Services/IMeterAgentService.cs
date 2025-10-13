cbf3ac4b07d70537235a18927dbd1b26a228396b
Matthew Rawson <Matthew.Rawson@infotrack.co.uk>
2025-10-13 19:17:07 +0100

    Add MeterAgentService

---
+++ meter-api/meter-api/Services/IMeterAgentService.cs
@@ -0,0 +1,13 @@
+﻿using meter_api.Datatypes;
+
+namespace meter_api.Services
+{
+    public interface IMeterAgentService
+    {
+        bool IsMeterAgentConnected(string meterId);
+        void AgentConnected(string meterId, string connectionId);
+        void AgentDisconnected(string meterId, string connectionId);
+        void HandleUsageUpdate(string meterId, AgentUsage usage);
+        void HandleErrorUpdate(string meterId, AgentError error);
+    }
+}
