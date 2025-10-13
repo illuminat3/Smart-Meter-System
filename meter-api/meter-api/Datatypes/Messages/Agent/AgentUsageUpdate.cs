ac9378c7b28c77c5d3dbbd0da1e1efd43056db67
Matthew Rawson <Matthew.Rawson@infotrack.co.uk>
2025-10-13 19:17:21 +0100

    Add Agent Messages

---
+++ meter-api/meter-api/Datatypes/Messages/Agent/AgentUsageUpdate.cs
@@ -0,0 +1,9 @@
+﻿namespace meter_api.Datatypes.Messages.Agent
+{
+    public class AgentUsageUpdate : IMessage<AgentUsage>
+    {
+        public string MessageName { get; } = "AgentUsageUpdate";
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public required AgentUsage Body { get; set; }
+    }
+}
