bee90e4aa00f5a087939530e32d535540c8c072d
Matthew Rawson <Matthew.Rawson@infotrack.co.uk>
2025-10-13 19:17:36 +0100

    Add body types

---
+++ meter-api/meter-api/Datatypes/AgentUsage.cs
@@ -0,0 +1,7 @@
+﻿namespace meter_api.Datatypes
+{
+    public class AgentUsage
+    {
+        public required double EnergyUsedKWh { get; set; } = 0.0d;
+    }
+}
---
+++ meter-api/meter-api/Datatypes/ConnectionMessage.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace meter_api.Datatypes
+{
+    public class ConnectionMessage
+    {
+        public required string MeterId { get; set; }
+        public required ConnectionState State { get; set; }
+    }
+}
