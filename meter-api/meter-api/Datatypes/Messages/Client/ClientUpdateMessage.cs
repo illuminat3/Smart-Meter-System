9f2608fc8bdf18b2703f77f8db483af8d1395b36
Matthew Rawson <Matthew.Rawson@infotrack.co.uk>
2025-10-13 19:17:16 +0100

    Add Client Messages

---
+++ meter-api/meter-api/Datatypes/Messages/Client/ClientUpdateMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace meter_api.Datatypes.Messages.Client
+{
+    public class ClientUpdateMessage : IMessage<MeterSnapshot>
+    {
+        public string MessageName { get; } = "MeterUpdate";
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public required MeterSnapshot Body { get; set; }
+    }
+}
