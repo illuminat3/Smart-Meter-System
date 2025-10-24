5ff15a443581d4319e48a487a72a3e7aeaa4a5db
Matthew <matthewrawson2@gmail.com>
2025-10-24 14:59:44 +0100

    61 add implementation for getusage function (#88)
    * add usage service implementation
    
    * Create usage generation

--- meter-agent/meter-agent/Services/UsageService.cs
+++ meter-agent/meter-agent/Services/UsageService.cs
@@ -2,9 +2,17 @@ namespace meter_agent.Services
 {
     public class UsageService : IUsageService
     {
+        private readonly Random _random = new();
+
         public decimal GetUsage()
         {
-            throw new NotImplementedException();
+            decimal min = 0.001m;
+            decimal max = 0.06m;
+
+            decimal usage = (decimal)_random.NextDouble();
+            usage = min + (usage * (max - min));
+
+            return usage;
         }
     }
 }
