160b4d1a9cc5f62fc101177e304945b90cbdc9ab
Illuminat3 <matthewrawson2@gmail.com>
2025-10-24 14:19:41 +0100

    remove primary constructor

--- meter-agent/meter-agent/Datatypes/Credentials.cs
+++ meter-agent/meter-agent/Datatypes/Credentials.cs
@@ -1,9 +1,9 @@
 ﻿namespace meter_agent.Datatypes
 {
-    public class Credentials(string meterId, string username, string password)
+    public class Credentials
     {
-        public required string MeterId { get; init; } = meterId;
-        public required string Username { get; init; } = username;
-        public required string Password { get; init; } = password;
+        public required string MeterId { get; init; }
+        public required string Username { get; init; }
+        public required string Password { get; init; }
     }
 }
