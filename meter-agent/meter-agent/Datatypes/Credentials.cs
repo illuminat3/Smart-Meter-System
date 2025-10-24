6975a9942dda452287d72a8695a8221aac0501cf
Illuminat3 <matthewrawson2@gmail.com>
2025-10-24 13:56:29 +0100

    Add Credential Object

---
+++ meter-agent/meter-agent/Datatypes/Credentials.cs
@@ -0,0 +1,9 @@
+﻿namespace meter_agent.Datatypes
+{
+    public class Credentials
+    {
+        public required string MeterId { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
