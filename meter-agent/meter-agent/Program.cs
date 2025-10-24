cbbfd1737128fcc1e52bf462adc6b57e3a0802e7
MatthewRawsonInfotrack <matthew.rawson@infotrack.co.uk>
2025-10-24 14:34:06 +0100

    64 extract login details from env file (#87)
    * add dotnet env
    
    * Create Env File
    
    * Add Credential Object
    
    * Read credentials
    
    * fix env file not being copied
    
    * Update credentials class
    
    * remove primary constructor
    
    ---------
    
    Co-authored-by: Illuminat3 <matthewrawson2@gmail.com>

--- meter-agent/meter-agent/Program.cs
+++ meter-agent/meter-agent/Program.cs
@@ -1,4 +1,6 @@
-﻿using meter_agent.Services;
+﻿using DotNetEnv;
+using meter_agent.Datatypes;
+using meter_agent.Services;
 
 namespace meter_agent
 {
@@ -8,7 +10,14 @@ namespace meter_agent
 
         public static void Main(string[] args)
         {
+            Env.Load();
 
+            var credentials = new Credentials
+            {
+                MeterId = Environment.GetEnvironmentVariable("METER_ID") ?? throw new Exception("METER_ID missing"),
+                Username = Environment.GetEnvironmentVariable("USERNAME") ?? throw new Exception("USERNAME missing"),
+                Password = Environment.GetEnvironmentVariable("PASSWORD") ?? throw new Exception("PASSWORD missing")
+            };
         }
     }
 }
