6794bfcc8bc7ea4fef018f87bb46b8f8429d9453
Matthew Rawson <Matthew.Rawson@infotrack.co.uk>
2025-10-31 15:40:23 +0000

    Add messages

---
+++ meter-agent/meter-agent/DataTypes/Messages/IMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace meter_agent.DataTypes.Messages
+{
+    public interface IMessage<TBody>
+    {
+        string MessageName { get; }
+        DateTime Timestamp { get; set; }
+        TBody Body { get; set; }
+    }
+}
