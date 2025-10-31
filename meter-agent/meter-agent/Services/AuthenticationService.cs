47d3144ea589b43b4809d7d51679c992efdd167c
Matthew Rawson <Matthew.Rawson@infotrack.co.uk>
2025-10-31 11:10:41 +0000

    add authentication service

---
+++ meter-agent/meter-agent/Services/AuthenticationService.cs
@@ -0,0 +1,32 @@
+﻿using meter_agent.Datatypes.Requests;
+using meter_agent.Datatypes.Responses;
+using System.Net.Http.Json;
+
+namespace meter_agent.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthenticationService(string apiBase)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(apiBase)
+            };
+        }
+
+        public async Task<AgentLoginResponse> Login(AgentLoginRequest request)
+        {
+            string path = "auth/agent/login";
+
+            var response = await _httpClient.PostAsJsonAsync(path, request);
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<AgentLoginResponse>();
+
+            return result ?? throw new InvalidOperationException("Failed to deserialize login response.");
+        }
+    }
+}
---
+++ meter-agent/meter-agent/Services/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using meter_agent.Datatypes.Requests;
+using meter_agent.Datatypes.Responses;
+
+namespace meter_agent.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<AgentLoginResponse> Login(AgentLoginRequest request);
+    }
+}
