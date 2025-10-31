94f67ba29924c157879fb35a13e8f0ab8d04fd95
Matthew Rawson <Matthew.Rawson@infotrack.co.uk>
2025-10-31 11:10:47 +0000

    add implementation of auth service

--- meter-agent/meter-agent/Program.cs
+++ meter-agent/meter-agent/Program.cs
@@ -1,16 +1,19 @@
 ﻿using DotNetEnv;
 using meter_agent.Datatypes;
+using meter_agent.Datatypes.Requests;
 using meter_agent.DataTypes.Exceptions;
 using meter_agent.Services;
+using System.Threading.Tasks;
 
 namespace meter_agent
 {
     public static class Program
     {
         private static readonly IUsageService usageService = new UsageService();
+        private static IAuthenticationService authenticationService;
         private static readonly Random random = new();
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Env.Load();
 
@@ -20,7 +23,19 @@ namespace meter_agent
                 Username = Environment.GetEnvironmentVariable("USERNAME") ?? throw new MissingCredentialException("USERNAME missing"),
                 Password = Environment.GetEnvironmentVariable("PASSWORD") ?? throw new MissingCredentialException("PASSWORD missing")
             };
+
+            var baseUrl = Environment.GetEnvironmentVariable("BASE_URL") ?? throw new MissingCredentialException("BASE_URL missing");
+
+            authenticationService = new AuthenticationService(baseUrl);
+
+            var loginRequest = new AgentLoginRequest
+            {
+                MeterId = credentials.MeterId,
+                Username = credentials.Username,
+                Password = credentials.Password
             };
+
+            var response = await authenticationService.Login(loginRequest);
 
             while (true)
             {
