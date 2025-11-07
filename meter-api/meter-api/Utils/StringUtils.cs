7a0543797777812d39307906f32d6e6d257687b0
Illuminat3 <matthewrawson2@gmail.com>
2025-11-07 15:11:44 +0000

    Create camelCase string util

---
+++ meter-api/meter-api/Utils/StringUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace meter_api.Utils
+{
+    public static class StringUtils
+    {
+        public static string ToCamelCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str) || char.IsLower(str[0]))
+                return str;
+
+            return char.ToLower(str[0]) + str.Substring(1);
+        }
+    }
+}
