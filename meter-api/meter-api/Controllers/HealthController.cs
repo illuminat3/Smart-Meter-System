25932836f2e323412c493dec181a3e50d6a7ecb0
Illuminat3 <matthewrawson2@gmail.com>
2025-11-24 19:18:37 +0000

    add health endpoint

---
+++ meter-api/meter-api/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace meter_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "Healthy" });
+        }
+    }
+}
